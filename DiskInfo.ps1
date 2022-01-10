2df807fe3d9758b43c6c3ed1c269554ddc72c79c
Kistappa <krishnayadahali937@gmail.com>
2022-01-10 12:08:54 +0530

    powershell notes

---
+++ DiskInfo.ps1
@@ -0,0 +1,12 @@
+﻿<#
+Add Comment 
+add here abou your description
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$True)]
+    [string[]]$ComputerName='localhost'
+)
+Get-WmiObject -ComputerName $ComputerName -Class win32_logicaldisk | select DeviceID, Size, Freespace
+
