4dec0d15727c83a95b57a79674c15eb53c28292c
Kistappa <krishnayadahali937@gmail.com>
2022-01-11 13:05:26 +0530

    added new lines in commands

---
+++ quotes.ps1
@@ -0,0 +1,12 @@
+﻿#Quotation Marks
+
+# double quotes resolve the variable
+$i="Powershell"
+
+"This is the varible $i, and $i Rocks!"
+# single quotes will not resolve variable
+'This is the varible $i, and $i Rocks!'
+"This is the varible $i, and $i Rocks!"
+
+$ComputerName="localhost"
+Get-Service -Name BITS -ComputerName $ComputerName | select MachineName, Name, Status
---
+++ vars.ps1
@@ -0,0 +1,16 @@
+﻿#variable to store your stuff
+#Assigning a varaible
+$MyVar=2
+${My Var}="Hello"
+
+#output a variable
+$MyVar
+${My Var}
+Write-Output $MyVar
+
+#strongly type a variable
+[string]$MyName="Kistappa"
+[int]$Comapany=1
+[validateset("a","b","c")][string]$x="a"
+[string]$ComputerName=Read-Host "Enter Computer Name"
+Write-Output $ComputerName
